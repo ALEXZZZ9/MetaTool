c3819ee39511c224722e01763c3677e33f32c2f6
alexzzz9 <alex@ax9.me>
2018-08-03 23:54:34 +0300

    Added simple tests

---
+++ MetaToolTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MetaToolTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MetaToolTest")]
+[assembly: AssemblyCopyright("Copyright © ALEXZZZ9 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("426cf16c-da86-43c7-8242-e4c55a2da541")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MetaToolTest/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MetaToolTest
+{
+    internal static class Utils
+    {
+        public static string Base64Decode(string text)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(text));
+        }
+
+        public static T XMLBase64Deserialize<T>(string xml)
+        {
+            return XMLDeserialize<T>(Base64Decode(xml));
+        }
+        public static T XMLDeserialize<T>(string xml)
+        {
+            TextReader reader = new StringReader(xml);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            T obj = (T)xmlSerializer.Deserialize(reader);
+            return obj;
+        }
+    }
+}
